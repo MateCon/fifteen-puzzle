459408b42512225dafe55615f893f649d9a3e20b
MateCon <68882595+MateCon@users.noreply.github.com>
2022-05-22 11:59:31 -0300

    work in about page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -66,19 +66,20 @@ const Navbar: FC = () => {
             >
                 {showTutorial && <>
                     <h2>About</h2>
-                    <p>This is a new version of the the classic game 15 puzzle of Game of Fifteen</p>
+                    <p>This is a new version of the the classic game 15 puzzle or Game of Fifteen</p>
                     <h2>Tutorial</h2>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <p style={{ marginRight: 10 }}>15 puzzle is a sliding puzzle consisting of an N by N board filled with numbers and one empty square</p>
+                        <p style={{ marginRight: 10 }}>15 puzzle is a sliding puzzle consisting of an N by N board filled with number squares and one empty square</p>
                         <img src={tutorial_1} style={{ width: '50%', marginTop: 5, marginBottom: 5 }} alt="initial game state" />
                     </div>
-                    <p style={{ marginTop: 10, marginBottom: 10 }}>You can move a number adjacent to the empty square into it by clicking it</p>
+                    <p style={{ marginTop: 10, marginBottom: 10 }}>You can move a number square adjacent to the empty square by clicking it</p>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                         <img src={tutorial_2} style={{ width: '50%', marginTop: 5, marginBottom: 5 }} alt="initial final state" />
-                        <p>The objective of the game is to get all the squares in numerical order from left to right and top to bottom</p>
+                        <p>The goal is to get all the squares in numerical order from left to right and top to bottom</p>
                     </div>
                     <hr />
-                    <p>Created by <a style={{ color: '#0fb9f2', textDecoration: 'underline' }} href="http://github.com/matecon" target="_blank" rel="norefferer noreferrer">MateCon</a></p>
+                    <p>Created and designed by <a style={{ color: '#0fb9f2', textDecoration: 'underline' }} href="http://github.com/matecon" target="_blank" rel="norefferer noreferrer">MateCon</a></p>
+                    <p>Sound design by <a style={{ color: '#0fb9f2', textDecoration: 'underline' }} href="https://www.pabloziffer.com/?gclid=Cj0KCQjwvqeUBhCBARIsAOdt45aCdjANzA8CT-tfXjLw5K7dDbhAtukYprVohusH-ttE5qGUvXzhsYYaAk97EALw_wcB" target="_blank" rel="norefferer noreferrer">Pablo Ziffer</a></p>
                 </>} <div />
             </Modal>
         </nav>
